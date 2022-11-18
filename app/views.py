b385a59cc310a35da2fc1f6e5251557ab5058dca
latishev_m <latishev_m@protei.ru>
2022-11-18 17:23:25 +0300

    Добавил файлы по ДЗ Работа с проектом и приложением

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from os import listdir
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now().strftime('%H:%M:%S')
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    template_name = 'app/workdir.html'
+    directory_contents = listdir()
+
+    context = {
+        'directory_contents': directory_contents
+    }
+    return render(request, template_name, context)
